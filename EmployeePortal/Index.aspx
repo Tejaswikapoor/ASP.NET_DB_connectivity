31bb7d55ad5dba975ddc99b37f94466097928987
Tejaswi kapoor <31186504+Tejaswikapoor@users.noreply.github.com>
2019-06-18 11:03:35 +0530

    Add project files.

---
+++ EmployeePortal/Index.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="EmployeePortal.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+           <h2>Welcome To DashBoard</h2>
+           
+            
+            <asp:TextBox ID="Textbox1" runat="server" OnTextChanged="Textbox1_TextChanged" style="height: 22px"   ></asp:TextBox>
+
+       
+      
+        
+        <asp:Button id="Button1" runat="server" text="submit" OnClick="Button1_click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ EmployeePortal/Index.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EmployeePortal
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (ViewState["clicks"] == null)
+                {
+                    ViewState["clicks"] = 0;
+                }
+                Textbox1.Text = ViewState["clicks"].ToString();
+
+            }
+        }
+            protected void Button1_click(object sender, EventArgs e)
+            {
+                int clickcount = (int)ViewState["clicks"] + 1;
+                Textbox1.Text = clickcount.ToString();
+                ViewState["clicks"] = clickcount;
+            }
+
+        protected void Textbox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
